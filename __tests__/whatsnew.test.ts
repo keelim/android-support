d726308536afe9b2d06f6a837232c2097958a0f0
Keel_im <kimh00335@gmail.com>
2024-06-25 23:41:27 +0900

    add ko-kr test case and delete other language

--- __tests__/whatsnew.test.ts
+++ __tests__/whatsnew.test.ts
@@ -2,29 +2,13 @@ import {readLocalizedReleaseNotes} from "../src/whatsnew";
 
 test("read localized whatsnew files", async () => {
     let texts = await readLocalizedReleaseNotes("./__tests__/whatsnew");
-    expect(texts).toHaveLength(6);
+    expect(texts).toHaveLength(2);
     expect(texts).toContainEqual({
         language: "en-US",
         text: "test_changelog_file"
     });
     expect(texts).toContainEqual({
-        language: "de-DE",
-        text: "test_changelog_file_german"
-    });
-    expect(texts).toContainEqual({
-        language: "ja-JP",
-        text: "test_changelog_file_japanese"
-    });
-    expect(texts).toContainEqual({
-        language: "ca",
-        text: "test_changelog_file_catalan"
-    });
-    expect(texts).toContainEqual({
-        language: "es-ES",
-        text: "test_changelog_file_spanish"
-    });
-    expect(texts).toContainEqual({
-        language: "et",
-        text: "test_changelog_file_estonian"
+        language: "ko-KR",
+        text: "test_changelog_file"
     });
 });
